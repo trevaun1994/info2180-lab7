a365c129f5bc0548c3a03e69b5816050597acb3b
trevaun1994 <trevaunam12@hotmail.com>
2016-12-15 17:03:29 -0500

    Updated PHP

--- world.php
+++ world.php
@@ -1,21 +1,15 @@
 <?php
-
-$name = $_GET["name"];   
-
-
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 $stmt = $conn->query("SELECT * FROM countries");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 echo '<ul>';
 foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
 }
 echo '</ul>';
+?>
